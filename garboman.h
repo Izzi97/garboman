5111e8ce60c53fd94c41bdf317dc98fabcc0eb75
Ismael Agchar <ismael.agchar@gmail.com>
2026-04-08 13:52:09 +0200

    add error handling

--- garboman.h
+++ garboman.h
@@ -10,6 +10,8 @@ typedef struct {
 	link_elem_t *top;
 } stack_t;
 
+void fail(char *r);
+
 stack_t* stack_new();
 void stack_push(stack_t *s, void *d);
 void alloc_stack_free(stack_t *s);
@@ -20,15 +22,22 @@ void garbo_free();
 
 #ifdef GARBOMAN_IMPL
 
+void fail(char *r) {
+	fputs(r, stderr);
+	exit(EXIT_FAILURE);
+}
+
 stack_t* stack_new() {
-	return calloc(1, sizeof(stack_t));
+	stack_t *s = calloc(1, sizeof(stack_t));
+	if (!s) fail("stack_new: allocation failed");
+	return s;
 }
 
 void stack_push(stack_t *s, void *d) {
-	if (!s || !d) exit(1);
+	if (!s || !d) fail("stack_push: stack or data invalid");
 
 	link_elem_t *le = malloc(sizeof(link_elem_t));
-	if (!le) exit(1);
+	if (!le) fail("stack_push: allocation of link element failed");
 
 	le->data = d;
 	le->other = s->top;
@@ -36,7 +45,7 @@ void stack_push(stack_t *s, void *d) {
 }
 
 void alloc_stack_free(stack_t *s) {
-	if (!s) exit(1);
+	if (!s) fail("alloc_stack_free: stack invalid");
 
 	link_elem_t *e = s->top, *o;
 	while (e) {
@@ -57,11 +66,11 @@ void garbo_track() {
 }
 
 void* garbo_alloc(size_t bytes) {
-	if (!ref_stack) exit(1);
-	if (!ref_stack->top) exit(1);
+	if (!ref_stack) fail("garbo_alloc: reference stack invalid");
+	if (!ref_stack->top) fail("garbo_alloc: reference stack empty");
 
 	void *r = malloc(bytes);
-	if (!r) exit(1);
+	if (!r) fail("garbo_alloc: allocation failed");
 
 	stack_push((stack_t*)ref_stack->top->data, r);
 	return r;
